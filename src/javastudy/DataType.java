ad238aa250a2bee410771d2179056eabd61ce716
Ngan Nguyen <ngannguyenthi224@gmail.com>
2023-05-23 17:38:25 +0700

    Data Type exercises

---
+++ src/javastudy/DataType.java
@@ -0,0 +1,33 @@
+package javastudy;
+
+public class DataType {
+
+	public static void main(String[] args) {
+
+		// HW1
+		System.out.println("HW1");
+		int a = 6;
+		int b = 2;
+		int P1 = a + b;
+		int P2 = a - b;
+		int P3 = a * b;
+		int P4 = a / b;
+
+		System.out.println("a+b=" + P1);
+		System.out.println("a-b=" + P2);
+		System.out.println("a*b=" + P3);
+		System.out.println("a/b=" + P4);
+
+		// HW2
+		System.out.println("HW2");
+		double L = 7.5;
+		double W = 3.8;
+		double P = L * W;
+		System.out.println("The rectangular are is " + P);
+
+		// HW3
+		System.out.println("HW3");
+		String h = "Hello Automation Testing";
+		System.out.println(h);
+	}
+}
